08cde471a9353a2116fca76efa4660a01bd5a044
Cereny <56936296+cerenyilmazjinx@users.noreply.github.com>
2025-08-02 20:32:50 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/nanoio/main.go
@@ -0,0 +1,23 @@
+// cmd/nanoio/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"nanoio/internal/nanoio"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := nanoio.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
